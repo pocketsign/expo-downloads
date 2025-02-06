8f4dee6643d020e3f45d41dce1711dd680856b8b
Ryo Kuramoto <me@deka.jp>
2025-02-06 17:14:55 +0900

    chore: pnpm create expo-module

---
+++ src/DownloadsModule.ts
@@ -0,0 +1,12 @@
+import { NativeModule, requireNativeModule } from 'expo';
+
+import { DownloadsModuleEvents } from './Downloads.types';
+
+declare class DownloadsModule extends NativeModule<DownloadsModuleEvents> {
+  PI: number;
+  hello(): string;
+  setValueAsync(value: string): Promise<void>;
+}
+
+// This call loads the native module object from the JSI.
+export default requireNativeModule<DownloadsModule>('Downloads');
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Reexport the native module. On web, it will be resolved to DownloadsModule.web.ts
+// and on native platforms to DownloadsModule.ts
+export { default } from './DownloadsModule';
+export { default as DownloadsView } from './DownloadsView';
+export * from  './Downloads.types';
